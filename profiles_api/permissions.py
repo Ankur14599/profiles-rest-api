6d46e872f7c6e914d2ea2304edb19782400c72a5
Ankur <ankur.prashar0286@gmail.com>
2023-02-01 23:05:27 +0530

    Added feed items

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -10,4 +10,11 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
         return obj.id == request.user.id
 
+class UpdateOwnStatus(permissions.BasePermission):
+    """Used for updating status in feed"""
 
+    def has_object_permission(self, request, view, obj):
+        """User is trying to update their own status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user_profile.id == request.user.id
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework import filters
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.settings import api_settings
@@ -114,3 +115,17 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 class UserProfileApiView(ObtainAuthToken):
     """Obtain auth token for login"""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication, )
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (
+            permissions.UpdateOwnStatus,
+            IsAuthenticated,
+        )
+
+    def perform_create(self, serializer):
+        """Sets the user profile to logged in user"""
+        serializer.save(user_profile=self.request.user)
